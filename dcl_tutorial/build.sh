303b5595fca005c90472c6b0b7366f261261d849
martinexner <martinexner@gmx.at>
2015-04-18 12:29:33 +0200

    updated dcl_tutorial

--- dcl_tutorial/build.sh
+++ dcl_tutorial/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if egrep -r "\\\-[a-z]" dcl_tutorial.tex; then
+	echo "looks like there are wrong hyphenations";
+	echo "\n\nfailed\n\n"
+	exit 1
+fi
+
 (
 	pdflatex dcl_tutorial &&
 #	makeindex -c -q "dcl_tutorial.idx" &&
