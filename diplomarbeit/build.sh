06a396947fa810c1366c0cbcd3f0be1456c58496
martinexner <martinexner@gmx.at>
2015-05-12 12:13:23 +0200

    build.sh update

--- diplomarbeit/build.sh
+++ diplomarbeit/build.sh
@@ -4,19 +4,29 @@ if [ "--force" != "$1" ]; then
 
 	if grep --color -ir "\\.}" content/*_glossary.tex; then
 		echo "\n\nERROR: don't end glossary descriptions with a dot\nnot built\nuse --force to override\n\n"
-		exit 1
+		if [ "--test" != "$1" ]; then
+			exit 1
+		fi
 	fi
 
 	if egrep --color -ir "section{.*\\\\gls.*}" content/*.tex; then
 		echo "\n\nERROR: don't use \\gls{...} in any sections\nnot built\nuse --force to override\n\n"
-		exit 1
+		if [ "--test" != "$1" ]; then
+			exit 1
+		fi
 	fi
 
 	if egrep --color -ir "caption{.*\\\\gls[^\\*].*}" content/*.tex; then
 		echo "\n\nERROR: use \\gls*{...} only in captions\nnot built\nuse --force to override\n\n"
-		exit 1
+		if [ "--test" != "$1" ]; then
+			exit 1
+		fi
 	fi
 
+fi
+
+if [ "--test" = "$1" ]; then
+	exit 0
 fi
 
 (
